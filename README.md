c1fbb6041b259800564940def1a25cc63edc4486
Edward J. Stronge <ejstronge@gmail.com>
2014-08-19 12:11:44 -0400

    Update run_downloader parameters
    Changed the run_downloader parameter list to be easier to read.
    Also added the output of `./run_downloader -h` to README.md

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # nlm_data_import
 
 ## Edward J. Stronge <ejstronge@gmail.com>
@@ -9,5 +8,35 @@ Used to collect data for [Hypothesis Graph](https://github.com/ejstronge/hypothe
 
 # Usage
 
+  usage: run_downloader.py [-h] [-n NETRC] [-l LIMIT] [-d DOWNLOAD_DATABASE]
+                           [-o OUTPUT_DIR] [-x EXPORT_DIR] [--email_debugging]
+                           [--from_email FROM_EMAIL] [--to_email TO_EMAIL]
+                           server_data_dir
+  
+  Script to download new files from the NLM public FTP server.
+  
+  positional arguments:
+    server_data_dir       Directory containing desired files on the NLM FTP
+                          server
+  
+  optional arguments:
+    -h, --help            show this help message and exit
+    -n NETRC, --netrc NETRC
+                          netrc file containing login parameters for the NLM
+                          server. See `man 5 netrc` for details on generating
+                          this file.
+    -l LIMIT, --limit LIMIT
+                          Only download LIMIT files.
+    -d DOWNLOAD_DATABASE, --download_database DOWNLOAD_DATABASE
+                          Path to SQLite database detailing past downloads
+    -o OUTPUT_DIR, --output_dir OUTPUT_DIR
+                          Directory where downloads will be saved
+    -x EXPORT_DIR, --export_dir EXPORT_DIR
+                          Directory where data to be retrieved by the
+                          `hypothesis_graph application server are staged.
+    --email_debugging     Send debugging emails. Defaults to FALSE.
+    --from_email FROM_EMAIL
+                          FROM field for debugging emails
+    --to_email TO_EMAIL   TO field for debugging emails
 
 (c) 2014, Edward J. Stronge. Released under the MIT License - see LICENSE.
--- run_downloader.py
+++ run_downloader.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 run_downloader.py
@@ -14,42 +15,46 @@ from nlm_data_import import download_nlm_data
 def handle_args():
     """Parse command-line arguments to this script"""
     parser = argparse.ArgumentParser(
-        description="""Script to download new files from the NLM public
+        description="""Script to download archives from the NLM public
                        FTP server.
                     """)
     # Server settings
-    parser.add_argument(
+    server_settings = parser.add_argument_group('FTP SERVER SETTINGS', '')
+    server_settings.add_argument(
         '-n', '--netrc', default='~/.netrc',
         help="""netrc file containing login parameters for the NLM
                 server. See `man 5 netrc` for details on generating this
-                file.
+                file or read nlm_data_import/netrc/example.netrc.
              """)
-    parser.add_argument(
+    server_settings.add_argument(
         'server_data_dir',
         help='Directory containing desired files on the NLM FTP server')
-    parser.add_argument(
+    server_settings.add_argument(
         '-l', '--limit', type=int, default=0,
         help='Only download LIMIT files.')
 
     # Download settings
-    parser.add_argument(
+    local_settings = parser.add_argument_group('LOCAL SETTINGS', '')
+    local_settings.add_argument(
         '-d', '--download_database', default='~/.ftp_download_db',
         help='Path to SQLite database detailing past downloads')
-    parser.add_argument(
+    local_settings.add_argument(
         '-o', '--output_dir', default='~/medline_data',
         help='Directory where downloads will be saved')
-    parser.add_argument(
+    local_settings.add_argument(
         '-x', '--export_dir', default='~/medline_data_exports',
         help="""Directory where data to be retrieved by the
                 `hypothesis_graph application server are staged.
              """)
-    # Sending debug emails
-    parser.add_argument(
+    # Sending debug emails (requires the send_ses_messages module - see
+    # setup.py)
+    debugging_settings = parser.add_argument_group('DEBUGGING SETTINGS', '')
+    debugging_settings.add_argument(
         '--email_debugging', default=False, action='store_true',
         help="Send debugging emails. Defaults to FALSE.")
-    parser.add_argument(
+    debugging_settings.add_argument(
         '--from_email', required=False, help="FROM field for debugging emails")
-    parser.add_argument(
+    debugging_settings.add_argument(
         '--to_email', required=False, help="TO field for debugging emails")
 
     return parser.parse_args()
